b68554c5140da96e48450f5e47d2f5af767010ef
Adriaan Beiertz <adriaanbd@gmail.com>
2019-03-05 02:33:05 -0500

    Adding select, all, any, none and partial count enums

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,6 +1,6 @@
 module Enumerable
   def my_each
-    return to_enum unless block_given?
+    return to_enum :my_each unless block_given?
 
     idx = 0
     while idx < length
@@ -11,23 +11,54 @@ module Enumerable
   end
 
   def my_each_with_index
-    return to_enum unless block_given?
+    return to_enum :my_each_with_index unless block_given?
 
     idx = 0
-    my_each do |item|
-      yield item, idx
-      idx += 1
-    end
+    my_each { |item| yield item, idx, idx += 1 }
     self
   end
+
+  def my_select
+    return to_enum :my_select unless block_given?
+
+    arr = []
+    my_each { |item| arr << item if yield(item) == true }
+    arr
+  end
+
+  def my_all?
+    my_each { |item| return false if yield(item) == false } if block_given?
+    my_each { |item| return false if !item } if !block_given?
+    true
+  end
+
+  def my_any?
+    my_each { |item| return true if yield(item) == true } if block_given?
+    my_each { |item| return true if item } if !block_given?
+    false
+  end
+
+  def my_none?
+    my_each { |item| return false if yield(item) != false } if block_given?
+    my_each { |item| return false if item } if !block_given?
+    true
+  end
+
+  def my_count(arg)
+    c = 0
+    my_each { |item| c += 1 if yield(item) == true } if block_given?
+    # my_each { |item| item == arg } if !block_given?
+    c
+  end 
 
   def my_map
     return to_enum unless block_given?
 
     arr = []
-    my_each do |item|
-      arr.push(yield(item))
-    end
+    my_each { |item| arr << yield(item) }
     arr
   end
+
+  def my_inject
+  end  
 end
